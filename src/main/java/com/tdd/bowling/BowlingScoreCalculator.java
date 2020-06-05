07b45fe25cb45dae1d6b9dc615e375d6ad47f21b
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-06-05 08:46:52 +0800

    calculate total score with all STRIKE round

--- src/main/java/com/tdd/bowling/BowlingScoreCalculator.java
+++ src/main/java/com/tdd/bowling/BowlingScoreCalculator.java
@@ -11,25 +11,23 @@ public class BowlingScoreCalculator {
   public static int calculateTotalScore(List<Integer> knockdownBottlesNumberList) {
 
     List<Integer> extraList = new ArrayList<>();
-    int round = 0;
-    for (int i = 0; i < knockdownBottlesNumberList.size() - 1 && round <= ROUND_NUMBER; i++) {
+    int round = 1;
+    for (int i = 0; i < knockdownBottlesNumberList.size() - 1 && round <= ROUND_NUMBER - 1; ) {
+      round++;
       Integer knockdownBottlesNumber = knockdownBottlesNumberList.get(i);
 
       if (knockdownBottlesNumber == NUMBER_OF_BOTTLES) {
         extraList.add(knockdownBottlesNumberList.get(i + 1));
         extraList.add(knockdownBottlesNumberList.get(i + 2));
         i++;
-        round++;
         continue;
       }
 
       Integer nextKnockdownBottlesNumber = knockdownBottlesNumberList.get(i + 1);
-
       if (knockdownBottlesNumber + nextKnockdownBottlesNumber == NUMBER_OF_BOTTLES) {
         extraList.add(knockdownBottlesNumberList.get(i + 2));
-        i++;
-        round++;
       }
+      i += 2;
     }
 
     extraList.addAll(knockdownBottlesNumberList);
